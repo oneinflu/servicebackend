9e7b9055ebb7fc76b01df5c4353ac5fc6e2458b9
oneinflu <hashtaginflu@gmail.com>
2025-08-23 11:29:58 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/register-admin', registerAdmin);
 router.post('/login', login);
+router.get('/profile', protect, getProfile);
 
 module.exports = router;
